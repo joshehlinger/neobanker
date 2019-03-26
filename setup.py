671554678a5e0d994859dc84c6fed8f04fe663a9
Josh Ehlinger <joshe@aweber.com>
2019-03-26 14:32:40 -0400

    minor adjustments

--- setup.py
+++ setup.py
@@ -4,17 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="neobanker",
-    version="0.0.1",
-    author="Joshua Ehlinger",
-    description="Collects neointerest",
+    name='neobanker',
+    install_requires=open('requirements.txt').read(),
+    description='Collects neointerest',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/joshehlinger/neobanker",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    py_modules=['neobanker'],
+    entry_points={
+        'console_scripts': [
+            'neobanker = neobanker:main'
+        ]
+    }
 )
